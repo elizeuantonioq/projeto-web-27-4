16369e13ed0065d5eb29cb91388a3a56f080b95e
Elizeu <elizeuantonioq@gmail.com>
2026-04-27 08:32:08 -0300

    chore:Criando a estrutura do projetoweb 3

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+//Metódo de limpeza de validação
+
+const email ="   fiap@email.com    ";
+const emailLimpo= email.trim(); //remove espaços (Início e fim)
+console.log(emailLimpo)
+
+// Validar:Verifica se o domínio é válido 
+
+if (emailLimpo.includes("@")){
+    console.log ('email válido')
+}else{
+    console.log("email inválido")
+}
+
+//Método de transformação de texto (split, tollowCase, Join)
+
+const tituloArtigo= "Aprendendo JavaScript RAIZ DEV"
+
+const texto1= tituloArtigo.split(" ");
+console.log(texto1)
+
+const texto2= tituloArtigo.toLowerCase();
+console.log(texto2);
+
+const texto3 =tituloArtigo.toUpperCase();
+console.log(texto3);
+
