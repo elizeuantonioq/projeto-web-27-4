125a230cafadd65b305e5acd021d836ed473029e
Elizeu <elizeuantonioq@gmail.com>
2026-04-27 10:01:44 -0300

    feat:aprendendo DOM

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,70 @@
-//Metódo de limpeza de validação
+//metodo de limpeza e validação 
 
-const email ="   fiap@email.com    ";
-const emailLimpo= email.trim(); //remove espaços (Início e fim)
-console.log(emailLimpo)
+const email ="  fiap@email.com    ";
+const emaillimp = email.trim(); //remove espaços 
+console.log(emaillimp)
 
-// Validar:Verifica se o domínio é válido 
+//Validar: verifica se o dominio é valido
 
-if (emailLimpo.includes("@")){
-    console.log ('email válido')
+
+if(emaillimp.includes("@")){
+    console.log("email válido")
 }else{
-    console.log("email inválido")
+    console.log("Email Inválido")
 }
 
-//Método de transformação de texto (split, tollowCase, Join)
+//métodos de transformação de texto (split, tolowcase, join)
 
-const tituloArtigo= "Aprendendo JavaScript RAIZ DEV"
+const tituloartigo = "aprendendo javascript RAIZ";
 
-const texto1= tituloArtigo.split(" ");
-console.log(texto1)
+const texto1 = tituloartigo.toLowerCase();
+console.log(texto1);
 
-const texto2= tituloArtigo.toLowerCase();
+const texto1_upper = tituloartigo.toUpperCase();
+console.log(texto1_upper);
+
+//transforma um texto em array(lista)
+const texto2 = tituloartigo.split(" ");
 console.log(texto2);
 
-const texto3 =tituloArtigo.toUpperCase();
+//o join faz o inverso, ele junta tudo em uma unica string
+const texto3 = texto2.join(" | ");
 console.log(texto3);
 
-//o join faz o inverso ,ele junta tudo em uma unica string
-const texto4 = texto1.join("*")
-console.log(texto4)
-
 //metodo tofixed
-const precoProduto=199.99;
-const desconto=0.15;
-const precoFinal=precoProduto *(1 - desconto)
-console.log(precoFinal)
-console.log(`R$ ${precoFinal.toFixed(2)}`)
+
+const precoproduto = 199.99;
+const desconto = 0.15;
+const precofinal = precoproduto * (1 - desconto);
+console.log(precofinal);
+console.log(`R$ ${precofinal.toFixed(2)}`)
+
+//Dom document object model
+// permite o javascript conversar com o navegador 
+//para manipular o html e css
+
+const titulo = document.getElementById("titulo");
+titulo.innerText ="D.O.M transformando o texto";
+titulo.style.color="blue";
+
+const produtos = [
+    {nome:"Teclado Gamer",preco:200,empromocao:true},
+    {nome:"Mouse Gamer",preco:150,empromocao:false},
+    {nome:"Monitor",preco:1300,empromocao:true},
+    {nome:"Padmouse",preco:50,empromocao:false},
+];
+
+const container =document.getElementById("lista-produtos");
+
+function produtoscard(lista){
+    const htmlprodutos= lista.map(item => ` 
+        <div class="card">
+            <h3>${item.nome}</h3> 
+            <p>Preço R$ ${item.preco}</p>
+            ${item.empromocao ? '<span>Promoção</span>': ''}
+        </div>
+        `).join('');//transforma o array em uma unica string
+
+        container.innerHTML=htmlprodutos;
+}
+produtoscard(produtos);
